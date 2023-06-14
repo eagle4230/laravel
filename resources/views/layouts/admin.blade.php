1dacbb0575ef725af58f779da9aa9d9ea5aae536
Artem <armrt@mail.ru>
2023-06-15 02:03:32 +0300

    Перенес sidebar, footer, header в анонимные компоненты

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
 
-  <title>Dashboard Template · Bootstrap v5.2</title>
+  <title>Admin</title>
 
   <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
 
@@ -71,47 +71,12 @@
 
 <body>
 
-  <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Company name</a>
-    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
-      data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search"
-      aria-label="Search">
-    <div class="navbar-nav">
-      <div class="nav-item text-nowrap">
-        <a class="nav-link px-3" href="#">Sign out</a>
-      </div>
-    </div>
-  </header>
+  <x-admin.header></x-admin.header>
 
   <div class="container-fluid">
     <div class="row">
-      <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-        <div class="position-sticky pt-3 sidebar-sticky">
-          <ul class="nav flex-column">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="{{ route('admin.admin.index') }}">
-                <span data-feather="home" class="align-text-bottom"></span>
-                Главная
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{ route('admin.categories.index') }}">
-                <span data-feather="file" class="align-text-bottom"></span>
-                Категории
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{ route('admin.news.index') }}">
-                <span data-feather="file" class="align-text-bottom"></span>
-                Новости
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+
+      <x-admin.sidebar></x-admin.sidebar>
 
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         @yield('content')
