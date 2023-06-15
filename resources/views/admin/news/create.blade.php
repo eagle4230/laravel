0de6a8f6ebc7f9d960d05335725b4f8020fb76e9
Artem <armrt@mail.ru>
2023-06-15 14:45:21 +0300

    Применил хелперы "old" и "routeIs"

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -8,27 +8,29 @@
   @csrf
   <div class="form-group">
     <label for="title">Заголовок</label>
-    <input type="text" name="title" id="title" class="form-control">
+    <input type="text" name="title" id="title" class="form-control" value="{{ old('title') }}">
   </div>
   <div class="form-group">
     <label for="author">Автор</label>
-    <input type="text" name="author" id="author" class="form-control">
+    <input type="text" name="author" id="author" class="form-control" value="{{ old('author') }}">
   </div>
   <div class="form-group">
     <label for="image">Изображение</label>
-    <input type="file" name="image" id="image" class="form-control">
+    <input type="file" name="image" id="image" class="form-control" value="{{ old('image') }}">
   </div>
   <div class="form-group">
     <label for="status">Статус</label>
     <select class="form-control" name="status" id="status">
-      <option>DRAFT</option>
-      <option>ACTIVE</option>
-      <option>BLOCKED</option>
+      <option @if (old('status')==='DRAFT' ) selected @endif>DRAFT</option>
+      <option @if (old('status')==='ACTIVE' ) selected @endif>ACTIVE</option>
+      <option @if (old('status')==='BLOCKED' ) selected @endif>BLOCKED</option>
     </select>
   </div>
   <div class="form-group">
     <label for="description">Описание</label>
-    <textarea name="description" class="form-control" id="description" cols="30" rows="5"></textarea>
+    <textarea name="description" class="form-control" id="description" cols="30" rows="5">
+      {!! old('description') !!}
+    </textarea>
   </div>
   <br>
   <button type="submit" class="btn btn-success">Сохранить</button>
