c20f6d4da877d93d2513a5e0d6fb77c3068dcfd7
Artem <armrt@mail.ru>
2023-07-23 17:58:22 +0300

    Настроил сохранение времени последней авторизации пользователя (пат.Наблюдатель)

--- app/Events/LoginEvent.php
+++ app/Events/LoginEvent.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
@@ -9,28 +11,30 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use App\Models\User;
 
 class LoginEvent
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+  use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    /**
-     * Create a new event instance.
-     */
-    public function __construct()
-    {
-        //
-    }
+  public User $user;
+  /**
+   * Create a new event instance.
+   */
+  public function __construct(User $user)
+  {
+    $this->user = $user;
+  }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
-    public function broadcastOn(): array
-    {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
-    }
+  /**
+   * Get the channels the event should broadcast on.
+   *
+   * @return array<int, \Illuminate\Broadcasting\Channel>
+   */
+  public function broadcastOn(): array
+  {
+    return [
+      new PrivateChannel('channel-name'),
+    ];
+  }
 }
